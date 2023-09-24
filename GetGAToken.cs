7f18814d14ee2a5eb2e69746170adb21f0b24f83
Ana Del Campo Mendizabal <andelcam@microsoft.com>
2023-09-24 15:02:40 -0400

    error loggin

--- GetGAToken.cs
+++ GetGAToken.cs
@@ -18,15 +18,27 @@ namespace GAAuth
     {
         [FunctionName("GetGAToken")]
             public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,ILogger log)            
         {
+            try{
+            log.LogInformation("C# HTTP trigger function processed a request.");
             var kvClient = new SecretClient(new Uri(Environment.GetEnvironmentVariable("KEY_VAULT_URL")), new ManagedIdentityCredential());
+            log.LogInformation("got key vault client");
+
             string keyJson = kvClient.GetSecret("GAAuthToken").Value.Value;
- 
+            log.LogInformation("got keyjson");
             var cred = GoogleCredential.FromJson(keyJson).CreateScoped(new string[] { "https://www.googleapis.com/auth/analytics.readonly" });
+            log.LogInformation("got credentials");
             var token = await cred.UnderlyingCredential.GetAccessTokenForRequestAsync();
+            log.LogInformation("token is " + token);
  
             return new OkObjectResult("{\"token\":\"" + token + "\"}");
+            }
+            catch(Exception ex)
+            {
+                log.LogError(ex.Message);
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
